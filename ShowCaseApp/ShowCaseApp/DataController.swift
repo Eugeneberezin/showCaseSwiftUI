03c1bba29dabd2778a22a8b331a88fd7a52fd435
Eugene Berezin <berezin.nlp@gmail.com>
2020-10-29 09:12:28 -0700

    First comit setup

---
+++ ShowCaseApp/ShowCaseApp/DataController.swift
@@ -0,0 +1,11 @@
+//
+//  DataController.swift
+//  ShowCaseApp
+//
+//  Created by Eugene Berezin on 10/29/20.
+//
+
+import CoreData
+import SwiftUI
+
+
---
+++ ShowCaseApp/ShowCaseApp/ShowCaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCaseAppApp.swift
+//  ShowCaseApp
+//
+//  Created by Eugene Berezin on 10/29/20.
+//
+
+import SwiftUI
+
+@main
+struct ShowCaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
