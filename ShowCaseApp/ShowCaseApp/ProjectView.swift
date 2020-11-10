d6a781a54934a91bd8ddd7f8cd330d70b259881b
Eugene Berezin <berezin.nlp@gmail.com>
2020-11-09 16:16:14 -0800

    Finish projectView

--- ShowCaseApp/ShowCaseApp/ProjectView.swift
+++ ShowCaseApp/ShowCaseApp/ProjectView.swift
@@ -18,54 +18,64 @@ struct ProjectView: View {
     
     @State private var showingSortOrder = false
     @State private var sortOrder = Item.SortOrder.optimized
- 
+    
     init(showClosedProjects: Bool) {
         self.showClosedProjects = showClosedProjects
         
-      projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [
-                NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)
-            ], predicate: NSPredicate(format: "closed = %d", showClosedProjects))
+        projects = FetchRequest<Project>(entity: Project.entity(), sortDescriptors: [
+            NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)
+        ], predicate: NSPredicate(format: "closed = %d", showClosedProjects))
         
     }
     
     var body: some View {
         
         NavigationView {
-            List {
-                    ForEach(projects.wrappedValue) { project in
-                        Section(header: ProjectHeaderView(project: project)) {
-                            ForEach(project.projectItems(using: sortOrder)) { item in
-                                ItemRowView(item: item)
-                            }
-                            .onDelete { offsets in
-                                let allItems = project.projectItems
-                                
-                                for offset in offsets {
-                                    let item = allItems[offset]
-                                    dataController.delete(item)
+            
+            Group {
+                if projects.wrappedValue.isEmpty {
+                    Text("There's nothing here right now")
+                        .foregroundColor(.secondary)
+                } else {
+                    
+                    List {
+                        ForEach(projects.wrappedValue) { project in
+                            Section(header: ProjectHeaderView(project: project)) {
+                                ForEach(project.projectItems(using: sortOrder)) { item in
+                                    ItemRowView(project: project, item: item)
+                                }
+                                .onDelete { offsets in
+                                    let allItems = project.projectItems(using: sortOrder)
+                                    
+                                    for offset in offsets {
+                                        let item = allItems[offset]
+                                        dataController.delete(item)
+                                    }
+                                    
+                                    dataController.save()
+                                    
                                 }
                                 
-                                dataController.save()
-                                
-                            }
-                            
-                            if showClosedProjects == false {
-                                Button {
-                                    withAnimation {
-                                        let item = Item(context: managedObjectContext)
-                                        item.project = project
-                                        item.creationDate = Date()
-                                        dataController.save()
+                                if showClosedProjects == false {
+                                    Button {
+                                        withAnimation {
+                                            let item = Item(context: managedObjectContext)
+                                            item.project = project
+                                            item.creationDate = Date()
+                                            dataController.save()
+                                        }
+                                    } label: {
+                                        Label("Add New Item", systemImage: "plus")
                                     }
-                                } label: {
-                                    Label("Add New Item", systemImage: "plus")
                                 }
                             }
                         }
                     }
+                    .listStyle(InsetGroupedListStyle())
                 }
-                .listStyle(InsetGroupedListStyle())
-                .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+            }
+            
+            .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     if showClosedProjects == false {
@@ -90,31 +100,35 @@ struct ProjectView: View {
                         Label("Sort", systemImage: "arrow.up.arrow.down")
                     }
                 }
-
+                
             }
             .actionSheet(isPresented: $showingSortOrder) {
                 ActionSheet(title: Text("Sort items"), message: nil, buttons: [
                     .default(Text("Optimized")) { sortOrder = .optimized},
                     .default(Text("Creation Date")) {sortOrder = .creationDate },
                     .default(Text("Title")) { sortOrder = .title}
-                
+                    
                 ])
             }
+            
+            SelectSomethingView()
         }
     }
     
- 
+    
+    
+    
 }
 
 struct ProjectView_Previews: PreviewProvider {
     
     static var dataController = DataController.preview
-      static var previews: some View {
+    static var previews: some View {
         ProjectView(showClosedProjects: false)
             .environment(\.managedObjectContext, dataController.container.viewContext)
-                .environmentObject(dataController)
+            .environmentObject(dataController)
     }
 }
-    
+
 
 
---
+++ ShowCaseApp/ShowCaseApp/SelectSomethingView.swift
@@ -0,0 +1,22 @@
+//
+//  SelectSomethingView.swift
+//  ShowCaseApp
+//
+//  Created by Eugene Berezin on 11/9/20.
+//
+
+import SwiftUI
+
+struct SelectSomethingView: View {
+    var body: some View {
+        Text("Please select something from the view")
+            .italic()
+            .foregroundColor(.secondary)
+    }
+}
+
+struct SelectSomethingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectSomethingView()
+    }
+}
