f0e85c11096f2f30e75c1a02be90b6db6f9daba8
Eugene Berezin <berezin.nlp@gmail.com>
2020-12-10 17:46:56 -0800

    HomeView

---
+++ ShowCaseApp/ShowCaseApp/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  ShowCaseApp
+//
+//  Created by Eugene Berezin on 12/10/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)    
+}
